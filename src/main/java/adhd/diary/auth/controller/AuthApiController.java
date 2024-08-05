6c580ddf5975e9a4f5b024989c7bb84cb6c5c251
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2024-08-05 18:51:42 +0900

    refactor: header 수정

--- src/main/java/adhd/diary/auth/controller/AuthApiController.java
+++ src/main/java/adhd/diary/auth/controller/AuthApiController.java
@@ -6,6 +6,7 @@ import adhd.diary.auth.jwt.JwtService;
 import adhd.diary.response.ApiResponse;
 import adhd.diary.response.ResponseCode;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -22,9 +23,13 @@ public class AuthApiController {
 
     @PostMapping("/api/auth/token/refresh")
     @Operation(summary = "refreshToken을 이용해 token을 재발급", description = "사용자의 토큰 정보가 만료되었을 경우 재발급 받기 위해 사용하는 API")
-    public ApiResponse<?> refreshAccessToken(HttpServletResponse response, @RequestHeader String authorizationHeader) {
-        System.out.println("go");
+    public ApiResponse<?> refreshAccessToken(HttpServletRequest request, HttpServletResponse response) {
         try {
+            String authorizationHeader = request.getHeader("Authorization");
+            if (authorizationHeader == null) {
+                throw new IllegalArgumentException("Authorization header is missing");
+            }
+
             String refreshToken = jwtService.extractRefreshTokenFromHeader(authorizationHeader);
             TokenResponse tokenResponse = jwtService.refreshTokens(refreshToken);
             jwtService.sendAccessAndRefreshToken(response, tokenResponse.getAccessToken(), tokenResponse.getRefreshToken());
