016e6790b2cf5ee2e01c28a139de51d44f372ff2
Sin Ye Rin <91180366+nyeroni@users.noreply.github.com>
2024-08-05 22:31:35 +0900

    refactor: cors 설정

--- src/main/java/adhd/diary/global/config/WebMvcConfig.java
+++ src/main/java/adhd/diary/global/config/WebMvcConfig.java
@@ -3,7 +3,6 @@ package adhd.diary.global.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.Arrays;
